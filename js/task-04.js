c523885bfd67426c0cf048483fd9f50609432f91
VladLyt96 <lytvyniuk.vladislav@gmail.com>
2023-11-27 21:55:56 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,33 @@
+const user = {
+  email: '',
+  password: '',
+  setEmail(newEmail) {
+    this.email = newEmail;
+  },
+  setPassword(newPassword) {
+    this.password = newPassword;
+  },
+};
 
+
+const getUserData = () => {
+  const registerForm = document.querySelector('.login-form');
+  registerForm.addEventListener('submit', handleSubmit);
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const login = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    if (login === '' || password === '') {
+      alert('All form fields must be filled in');
+    } 
+      user.setEmail(login);
+      user.setPassword(password)
+      console.log(user);
+      form.reset();
+    
+  }
+ 
+};
+
+getUserData()

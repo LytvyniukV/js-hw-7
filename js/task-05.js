b32608d17c9c33ed525015d8d3b7c95429ec07ad
VladLyt96 <lytvyniuk.vladislav@gmail.com>
2023-11-28 19:41:20 +0100

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,13 @@
+let randomColor;
+const button = document.querySelector('.change-color');
+const colorText = document.querySelector('.color');
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  randomColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+
+  colorText.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
 }
+
+button.addEventListener('click', getRandomHexColor);
